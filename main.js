00e91d312a8769aa5bd3fb03860771225c6f5507
maka y <nord2sudjp@gmail.com>
2020-11-14 18:36:15 +0900

    Create default window

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+// console.log("Hello");
+
+const { app, BrowserWindow } = require('electron')
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'ImageShrink',
+        width: 500,
+        height : 600
+    })
+}
+
+app.on('ready', createMainWindow)
