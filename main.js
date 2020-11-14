886b1f129064e9fb72bffc334423c14a1675e0a3
maka y <nord2sudjp@gmail.com>
2020-11-14 19:14:10 +0900

    Add Main Menu

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require("electron");
 
-process.env.NODE_ENV="development"
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
+process.env.NODE_ENV = "development";
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
 
 //console.log(process.platform)
-const isMac = process.platform === 'darwin' ? true : false
+const isMac = process.platform === "darwin" ? true : false;
 
-let mainWindow
-
+let mainWindow = null;
 
 function createMainWindow() {
-    const mainWindow = new BrowserWindow({
-        title: 'ImageShrink',
-        width: 500,
-        height: 600,
-        icon: './assets/icons/Icon_256x256.png',
-        resizable: isDev ? true : false
-    })
+  mainWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 500,
+    height: 600,
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: isDev ? true : false,
+  });
 
-    // mainWindow.loadURL("https://www.google.com")
-    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
-    
-
+  // mainWindow.loadURL("https://www.google.com")
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
 }
+app.on("ready", () => {
+  createMainWindow();
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindow.on("ready", () => {
+    mainWindow = null;
+  });
+});
+const menu = [
+  { label: "File", submenu: [{ label: "Quit", click: () => app.quit }] },
+];
 
-app.on('ready', createMainWindow)
-
-
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   if (!isMac) {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
