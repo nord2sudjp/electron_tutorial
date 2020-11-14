8489f3c559dc5332bdf61289abcbbb97bf997d7b
maka y <nord2sudjp@gmail.com>
2020-11-14 18:42:41 +0900

    load local index.html

--- main.js
+++ main.js
@@ -2,12 +2,20 @@
 
 const { app, BrowserWindow } = require('electron')
 
+let mainWindow
+
+
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'ImageShrink',
         width: 500,
         height : 600
     })
+
+    // mainWindow.loadURL("https://www.google.com")
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+    
+
 }
 
 app.on('ready', createMainWindow)
