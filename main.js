a7d21f469602dc3c87ed52a707e4fa2e81992cb6
maka y <nord2sudjp@gmail.com>
2020-11-14 19:22:18 +0900

    Menu & Globalshortcut

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
 
 process.env.NODE_ENV = "development";
 const isDev = process.env.NODE_ENV !== "production" ? true : false;
@@ -6,7 +6,7 @@ const isDev = process.env.NODE_ENV !== "production" ? true : false;
 //console.log(process.platform)
 const isMac = process.platform === "darwin" ? true : false;
 
-let mainWindow = null;
+let mainWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -15,6 +15,7 @@ function createMainWindow() {
     height: 600,
     icon: "./assets/icons/Icon_256x256.png",
     resizable: isDev ? true : false,
+    backgroundColor: "white",
   });
 
   // mainWindow.loadURL("https://www.google.com")
@@ -24,12 +25,24 @@ app.on("ready", () => {
   createMainWindow();
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
+  globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
+  globalShortcut.register("Ctrl+Shift+I", () => mainWindow.toggleDevTools());
+
   mainWindow.on("ready", () => {
     mainWindow = null;
   });
 });
 const menu = [
-  { label: "File", submenu: [{ label: "Quit", click: () => app.quit }] },
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        accelerator: "CmdOrCtrl+W",
+        click: () => app.quit(),
+      },
+    ],
+  },
 ];
 
 app.on("window-all-closed", () => {
