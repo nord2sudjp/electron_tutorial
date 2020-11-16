bf56da06e111a5056be01b9caabc361bcbf53e26
maka y <nord2sudjp@gmail.com>
2020-11-16 21:37:34 +0900

    implement shrink

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const path = require("path");
+const os = require("os");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+const imagemin = require("imagemin");
+const imageminPngquant = require("imagemin-pngquant");
+// const imageminMozjpeg = require("imageminmozjpeg");
+const slash = require("slash");
 
 process.env.NODE_ENV = "development";
 const isDev = process.env.NODE_ENV !== "production" ? true : false;
@@ -74,8 +80,33 @@ const menu = [
 ];
 
 ipcMain.on("image:minimize", (e, options) => {
-  console.log(options);
+  console.log("ipcMain.on: ", options);
+
+  options.dest = path.join(os.homedir(), "imageshrink");
+  shrinkImage(options);
 });
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    });
+    console.log("shrinkImage:", files);
+    shell.openItem(dest);
+
+    mainWindow.webContents.send("image:done");
+  } catch (err) {
+    console.log("shrinkImage@err:", err);
+    mainWindow.webContents.send("image:err");
+  }
+}
 
 app.on("window-all-closed", () => {
   if (!isMac) {
